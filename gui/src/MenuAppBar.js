166ab02fc24613d66b5294900b7084ec35038f48
Juliano Fischer Naves <julianofischer@gmail.com>
2022-11-11 20:52:30 -0300

    router

--- gui/src/MenuAppBar.js
+++ gui/src/MenuAppBar.js
@@ -1,15 +1,27 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Menu from "@mui/icons-material/Menu";
+import MenuItem from "@mui/icons-material/Menu";
+import { Fade } from "@mui/material";
+import SignUp from "./SignUp";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 export default function MenuAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
+      <Router>
       <AppBar position="static">
         <Toolbar>
           <IconButton
@@ -21,12 +33,21 @@ export default function MenuAppBar() {
           >
             <MenuIcon />
           </IconButton>
+
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            Relatório de Atividades Remotas
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Link to="/login"><Button color="inherit">Login</Button></Link>
         </Toolbar>
       </AppBar>
+        
+        <Switch>
+          <Route path="/login">
+            <SignUp />
+          </Route>
+        </Switch>
+
+      </Router>
     </Box>
   );
 }
